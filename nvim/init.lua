b78f22f159232ba902322b795d16b698dcaff7cc
Silvia Guerrero <silviasookie@gmail.com>
2023-12-25 17:24:18 +0100

    feat: coc

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,58 +12,80 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	{ 
-	  "cocopon/iceberg.vim",
-	  config = function()
-	    vim.cmd("colorscheme iceberg")
-	  end
-	},
-	{
-	  "nvim-treesitter/nvim-treesitter",
-	  build = ":TSUpdate",
-	  config = function() 
-		--require 'nvim-treesitter.install'.compilers = {"clang"} -- Must use llvm `scoop install llvm` https://github.com/nvim-treesitter/nvim-treesitter/issues/1897
+  {
+    "cocopon/iceberg.vim",
+    config = function()
+      vim.cmd("colorscheme iceberg")
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      --require 'nvim-treesitter.install'.compilers = {"clang"} -- Must use llvm `scoop install llvm` https://github.com/nvim-treesitter/nvim-treesitter/issues/1897
 
-		require'nvim-treesitter.configs'.setup {
-		  ensure_installed = {
-		    "c",
-		    "lua",
-		    "vim",
-		    "vimdoc",
-		    "query",
-		    "javascript",
-		    "typescript",
-		    "vue",
-		    "java",
-		    "php",
-		    "html",
-		    "css",
-		    "scss",
-		    "json",
-		    "yaml",
-		    "toml",
-		    "bash",
-		    "python",
-		    "rust",
-		    "dockerfile",
-		    "latex",
-		  },
+      require 'nvim-treesitter.configs'.setup {
+        ensure_installed = {
+          "c",
+          "lua",
+          "vim",
+          "vimdoc",
+          "query",
+          "javascript",
+          "typescript",
+          "vue",
+          "java",
+          "php",
+          "html",
+          "css",
+          "scss",
+          "json",
+          "yaml",
+          "toml",
+          "bash",
+          "python",
+          "rust",
+          "dockerfile",
+          "latex",
+        },
 
-		  auto_install = true,
+        auto_install = true,
 
-		  highlight = {
-		    enable = true,
-		    additional_vim_regex_highlighting = false,
-		    disable = {
-		      "latex", -- Vimtex already does this
-		    },
-		  },
-		}
-	  end
-	}
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+          disable = {
+            "latex", -- Vimtex already does this
+          },
+        },
+      }
+    end
+  },
+  {
+    "neoclide/coc.nvim",
+    branch = "release",
+    config = function()
+      vim.g["coc_global_extensions"] = {
+        "coc-sumneko-lua",
+        "coc-json"
+      }
+
+      vim.cmd([[
+	    	inoremap <silent><expr> <tab> pumvisible() ? coc#_select_confirm() : "\<C-g>u\<TAB>"
+		inoremap <silent><expr> <cr> "\<c-g>u\<CR>"
+		nnoremap <silent> K :call ShowDocumentation()<CR>
+	    ]])
+    end
+  }
 }
 
 local opts = {}
 
 require("lazy").setup(plugins, opts)
 
+vim.cmd([[
+set expandtab
+set tabstop=2
+set shiftwidth=2
+set autoindent
+]])
